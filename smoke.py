036829b8d236c770620402431d380aeeeb6444c2
Neoon <neo@tiefkuehler-madness.me>
2021-07-24 17:15:26 +0200

    base

---
+++ smoke.py
@@ -0,0 +1,62 @@
+import json
+
+with open("config.json") as handle:
+    config = json.loads(handle.read())
+
+#print(config)
+
+file = '''*** Targets ***
+
+probe = FPing
+
+menu = Top
+title = Network Latency Grapher
+remark = Welcome to the SmokePing website of xxx Company. \
+         Here you will learn all about the latency of our network.
+
++ World
+menu = World
+title = World
+
+'''
+
+for target,targets in config['targets'].items():
+    file += '''++ '''+target+'''
+menu = '''+target+'''
+title = '''+target+'''
+    '''
+    for destination,data in targets.items():
+        file += '''
++++ '''+destination+'''
+
+menu = '''+data['title']+'''
+title = '''+data['title']+'''
+host = '''+data['target']+'''
+
+'''
+
+file += '''++ Remote
+menu = Remote
+title = Remote
+
+'''
+
+for remote,data in config['remotes'].items():
+    file += '''+++ '''+remote+'''
+menu = '''+data['title']+'''
+title = '''+data['title']+'''
+
+'''
+    for target,targets in config['targets'].items():
+        for destination,details in targets.items():
+            file += '''++++ '''+destination+'''
+
+menu = '''+details['title']+'''
+title = '''+details['title']+'''
+probe = '''+data['probe']+'''
+host = '''+details['target']+'''
+
+'''
+
+with open("config", 'w') as out:
+    out.write(file)
